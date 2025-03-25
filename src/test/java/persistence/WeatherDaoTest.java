f987ddb28a060faccbaada07eff2672943889f32
Jake Cipri <ciprij97@gmail.com>
2025-03-25 14:26:46 -0500

    WeatherDaoTest created

---
+++ src/test/java/persistence/WeatherDaoTest.java
@@ -0,0 +1,14 @@
+package persistence;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WeatherDaoTest {
+
+    @Test
+    public void getResponseSuccess() {
+        WeatherDao dao = new WeatherDao();
+        assertEquals("Sun Prairie", dao.getWeather("53590").getLocation().getName());
+    }
+}
